d69caf94933bd751deb93f130e0e9f423e534ead
Cristian Souza <cmsouzaac@outlook.com>
2024-12-23 15:37:46 -0500

    modificando variaaveis

--- Variaveis.py
+++ Variaveis.py
@@ -8,4 +8,35 @@
 
 # Inteiro
 idade = 25
-print(idade);
+print(idade)
+
+# Float
+altura = 1.75
+print(altura)
+
+# String
+nome = "Cristian"
+print(nome)
+
+# Booleano
+ativo = True
+print(ativo)
+
+# Lista
+numeros = [1, 2, 3, 4]
+print(numeros)
+
+# Dicionário
+dados = {"nome": "Cristian", "idade": 37}
+print(dados)
+
+# None (variável sem valor)
+vazia = None
+print(vazia)
+
+# Atribuição Multiplas
+a, b, c = 1, 2, 3
+
+# Atribuir o mesmo valor a varias variaveis
+x = y = z = 0
+print(x, y, z)
