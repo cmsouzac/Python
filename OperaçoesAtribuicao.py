ead0cade67eaf575c77fb2495b8231c47803423d
Cristian Souza <cmsouzaac@outlook.com>
2024-12-23 19:49:16 -0500

    atribuição

---
+++ OperaçoesAtribuicao.py
@@ -0,0 +1,10 @@
+# Atribuindo os valores iniciais
+a = 1
+b = 1
+# Somando 1 + 1
+resultado = a + b
+print("Soma inicial:", resultado)  # Exibe 2
+
+# Incrementando o resultado para dar 3
+resultado += 1  # Equivale a resultado = resultado + 1
+print("Após incrementar:", resultado)  # Exibe 3
---
+++ operacoes.py
@@ -0,0 +1,16 @@
+# Atribuindo valores às variáveis
+a = 1
+b = 1
+
+# Imprimindo a soma diretamente
+print(a + b)
+
+# Imprimindo os valores de 'a' e 'b' individualmente
+print(a, b)
+
+# Atribuindo os valores iniciais
+a = 1
+b = 1
+
+
+
