f558e3ab66c770be46a9401db8c93093d70111ed
Cristian Souza <cmsouzaac@outlook.com>
2024-12-27 21:23:18 -0500

    exercicios e tipos de dados

--- TiposDeDados.py
+++ TiposDeDados.py
@@ -1,24 +1,38 @@
 # Exemplo 1: Inteiro
 x = 10
 print(type(x))  # Saída: <class 'int'>
+print(x)
 
 # Exemplo 2: Float
 y = 3.14
 print(type(y))  # Saída: <class 'float'>
+print(y)
+
+# Exemplo 2.1: Float
+y1 = .7
+print(type(y))  # Saída: <class 'float'>
+print(y1)
 
 # Exemplo 3: String
 z = "Olá, mundo!"
 print(type(z))  # Saída: <class 'str'>
+print(z)
 
 # Exemplo 4: Lista
 lista = [1, 2, 3]
 print(type(lista))  # Saída: <class 'list'>
+print(lista)
 
 # Exemplo 5: Dicionário
 dicionario = {"chave": "valor"}
 print(type(dicionario))  # Saída: <class 'dict'>
+print(dicionario)
 
 # Exemplo 6: Exponeciação
 exp = 2. ** 3
 print(exp)
-print(type(exp)) # Saída: <class 'Exp'>
+print(type(exp)) # Saída: <class 'Float'>
+
+# Exemplo 6.1: Exponeciação
+esp1 = 2e-3 # 2 * 10 ** -3
+print(esp1) # Saída: <class 'Float'>
