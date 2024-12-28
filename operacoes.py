cdddf8698abb5157d40c9599c74ca3ce68e4eb82
Cristian Souza <cmsouzaac@outlook.com>
2024-12-27 22:54:33 -0500

    Inclusão em operações matematicas

--- operacoes.py
+++ operacoes.py
@@ -1,13 +1,46 @@
+### Operações Matematica
+
+#   Adição: a + b
+#   Subtração: a - b
+#   Multiplicaçao: a * b
+#   Divisão: a / b
+# #
+
 # Atribuindo valores às variáveis
-a = 1
-b = 1
+a = 5
+b = 3
 
 # Imprimindo a soma diretamente
 print(a + b)
 
+print(a - b)
+
+print(a * b)
+
+print(a / b)
+
 # Imprimindo os valores de 'a' e 'b' individualmente
 print(a, b)
 
+### Operações Avançadas
+
+#   Divisão Inteira: a // b 
+#   Modulo (Resto da Divisão): a % b
+#   Exponenciação: a ** b
+#   Operações de Comparação: ==, !=, <, <=, >, >=
+# #
+
+print(a // b)
+print(a % b) 
+print(a ** b)
+
+### Operações Por comparação
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
 
 
 
