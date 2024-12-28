2dc2b1c0b9be1abfac27250e710a4bf62534f492
Cristian Souza <cmsouzaac@outlook.com>
2024-12-27 22:27:19 -0500

    solução do exercicio

--- Ex1.py
+++ Ex1.py
@@ -1,6 +1,18 @@
+nome = 'Cristian'
 print('Nome:', nome)
+
+sobrenome = 'Souza'
 print('Sobrenome:', sobrenome)
+
+idade = 37
 print('Idade:', idade)
+
+ano_nascimento = 1987
 print('Ano de nascimento:', ano_nascimento)
+
+maior_de_idade = True
 print('É maior de idade?', maior_de_idade)
+print(type(maior_de_idade))
+
+altura_metros = 187
 print('Altura em metros:', altura_metros)
